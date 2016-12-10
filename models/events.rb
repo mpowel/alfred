5f404d6a983582b8bbdd0468cceace61389ad4cd
mpowel <mlp206@gmail.com>
2016-12-10 15:51:14 -0500

    bundle install

---
+++ models/events.rb
@@ -0,0 +1,14 @@
+class Event < ActiveRecord::Base
+  
+  #has_many :tasks, dependent: :destroy
+  belongs_to :team
+    
+  validates_presence_of :team_id
+
+  def formatted_text
+    text.downcase.strip
+  end    
+
+  
+end
+Contact GitHub API Training Shop Blog About
