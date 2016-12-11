aa533b60e38b7791047950dd05d6deacf62c4030
mpowel <mlp206@gmail.com>
2016-12-10 23:36:49 -0500

    .

--- helpers/commands_helpers.rb
+++ helpers/commands_helpers.rb
@@ -32,18 +32,18 @@ module Sinatra
       client.chat_postMessage(channel: event.channel, text: "You're very welcome.", as_user: true)
 ## How do I generate a random list of thank yous?      
 
-        # Handle the Help commands
-      elsif event.formatted_text.include? "help"
-        client.chat_postMessage(channel: event.channel, text: "I can store contacts in my database. Would you like to create a new contact? Type 'yes' or no'.", as_user: true)
-
-#Later I want to add  "and soon will be able to track interactions with the email account etanproject.org@gmail.com"
-        
-      elsif event.formatted_text == "yes"
-              client.chat_postMessage(channel: event.channel, text: "Who would you like to add? Type `add [name]` and I'll add them for you.", as_user: true)
-
-      elsif event.formatted_text == "no"
-              client.chat_postMessage(channel: event.channel, text: "Ok, maybe later. Type 'view contacts' to see a list of existing contacts, otherwise I’ll come back later.", as_user: true)
-     
+#         # Handle the Help commands
+#       elsif event.formatted_text.include? "help"
+#         client.chat_postMessage(channel: event.channel, text: "I can store contacts in my database. Would you like to create a new contact? Type 'yes' or no'.", as_user: true)
+#
+# #Later I want to add  "and soon will be able to track interactions with the email account etanproject.org@gmail.com"
+#
+#       elsif event.formatted_text == "yes"
+#               client.chat_postMessage(channel: event.channel, text: "Who would you like to add? Type `add [name]` and I'll add them for you.", as_user: true)
+#
+#       elsif event.formatted_text == "no"
+#               client.chat_postMessage(channel: event.channel, text: "Ok, maybe later. Type 'view contacts' to see a list of existing contacts, otherwise I’ll come back later.", as_user: true)
+#
 # ------------------------------------------------------------------------
 # =>   MOVED TO CREATE CONTACT
 # ------------------------------------------------------------------------   
@@ -105,11 +105,11 @@ module Sinatra
       # #   ...
       # # add additional commands here...
                              
-      else
-        # ERROR Commands
-        # not understood or an error
-        client.chat_postMessage(channel: event.channel, text: "I didn't get that. If you're stuck, type `help` to find my commands.", as_user: true)
-        
+      # else
+      #   # ERROR Commands
+      #   # not understood or an error
+      #   client.chat_postMessage(channel: event.channel, text: "I didn't get that. If you're stuck, type `help` to find my commands.", as_user: true)
+      #
       end
       
     end
