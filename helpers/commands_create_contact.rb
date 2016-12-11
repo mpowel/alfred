1eee545f3cd96dcf0fb2fe746ddb4d0e1431f5ab
mpowel <mlp206@gmail.com>
2016-12-11 01:51:15 -0500

    working! adding is_a? Integer back in

--- helpers/commands_create_contact.rb
+++ helpers/commands_create_contact.rb
@@ -79,14 +79,14 @@ module Sinatra
           client.chat_postMessage(channel: event.channel, text: "What's her phone number? ", as_user: true)
         end
 
-       #  if event.formatted_text.is_a? Integer
-      #        # if formatted_number { |b| event.formatted_text b }
-      #
-      #   contact = Contact.all.last
-      #   contact.phone = event.formatted_text  #.convert_to_phone.format_phone
-      #   contact.save!
-      #
-      #   client.chat_postMessage(channel: event.channel, text: "I've updated _#{ contact.name }_'s phone number as #{contact.phone}.", as_user: true)
+      elsif event.formatted_text.is_a? Integer
+             # if formatted_number { |b| event.formatted_text b }
+
+        contact = Contact.all.last
+        contact.phone = event.formatted_text  #.convert_to_phone.format_phone
+        contact.save!
+
+        client.chat_postMessage(channel: event.channel, text: "I've updated _#{ contact.name }_'s phone number as #{contact.phone}.", as_user: true)
       # end
       # add additional commands here...
       
