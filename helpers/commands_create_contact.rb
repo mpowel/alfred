494f840c495e427838f4236660313124967ef0b7
mpowel <mlp206@gmail.com>
2016-12-11 01:46:30 -0500

    revert

--- helpers/commands_create_contact.rb
+++ helpers/commands_create_contact.rb
@@ -103,18 +103,18 @@ module Sinatra
     # ------------------------------------------------------------------------
     # =>   GETS USEFUL INFO FROM SLACK
     # ------------------------------------------------------------------------
-class String  # I don't really know how to use class
-    def convert_to_phone
-       number = self.gsub(/\D/, '').split(//)
-       #US 11-digit numbers
-       number = number.drop(1) if (number.count == 11 && number[0] == 1)
-       #US 10-digit numbers
-       number.to_s if (number.count == 10)
-     end
-     def format_phone
-       return "#{self[0,3]}-#{self[3,3]}-#{self[6,4]}"
-     end
-   end
+# class String  # I don't really know how to use class
+#     def convert_to_phone
+#        number = self.gsub(/\D/, '').split(//)
+#        #US 11-digit numbers
+#        number = number.drop(1) if (number.count == 11 && number[0] == 1)
+#        #US 10-digit numbers
+#        number.to_s if (number.count == 10)
+#      end
+#      def format_phone
+#        return "#{self[0,3]}-#{self[3,3]}-#{self[6,4]}"
+#      end
+#    end
     def format_number(number)
         digits = number.gsub(/\D/, '').split(//)
 
