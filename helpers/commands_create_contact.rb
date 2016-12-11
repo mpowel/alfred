91d74e37bcdc0f4cd65da561e69c6994adb0989b
mpowel <mlp206@gmail.com>
2016-12-11 01:17:06 -0500

    .

--- helpers/commands_create_contact.rb
+++ helpers/commands_create_contact.rb
@@ -87,7 +87,7 @@ module Sinatra
 
         client.chat_postMessage(channel: event.channel, text: "I've updated _#{ contact.name }_'s phone number.", as_user: true)
 
-        end
+  
       # add additional commands here...
         end
       else
