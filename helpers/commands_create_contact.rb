d659d911d47d8897a6b714d533d7a46133df43c2
mpowel <mlp206@gmail.com>
2016-12-11 01:52:49 -0500

    close tag

--- helpers/commands_create_contact.rb
+++ helpers/commands_create_contact.rb
@@ -79,7 +79,7 @@ module Sinatra
           client.chat_postMessage(channel: event.channel, text: "What's her phone number? ", as_user: true)
         end
 
-      elsif event.formatted_text.is_a? Integer
+        if event.formatted_text.is_a? Integer   #if or elsif??
              # if formatted_number { |b| event.formatted_text b }
 
         contact = Contact.all.last
@@ -87,8 +87,9 @@ module Sinatra
         contact.save!
 
         client.chat_postMessage(channel: event.channel, text: "I've updated _#{ contact.name }_'s phone number as #{contact.phone}.", as_user: true)
-      # end
-      # add additional commands here...
+        end
+        
+        # add additional commands here...
       
       else
         # ERROR Commands
