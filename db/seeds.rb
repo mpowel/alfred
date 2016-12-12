acba35956ead7b92d2251c1b426ce1340af5c4cc
mpowel <mlp206@gmail.com>
2016-12-11 20:17:16 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,78 +4,78 @@ Contact.create!([{
     title: "Butler",
     email: "alfred@botler.com",
     phone: 1235551212  
-  },
-  {
+  }])
+Contact.create!([{
     name: "Butler Livers",
     gender: "male",
     title: "Butler",
     email: "livers@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "Edwin Jarvis".
     gender: "male",
     title: "Butler",
     email: "jarvis@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "Geoffrey Butler".
     gender: "male",
     title: "Butler",
     email: "geoffrey@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "Herbert Arthur Runcible Cadbury",
     gender: "male",
     title: "Butler",
     email: "hcadbury@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "Ianto Jones",
     gender: "male",
     title: "Butler",
     email: "ianto@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "Orto K. Gotchi",
     gender: "male",
     title: "Butler",
     email: "orto@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "Rosie the Robot Maid",
     gender: "female",
     title: "Maid",
     email: "rosie@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "Sebastian",
     gender: "male",
     title: "Butler",
     email: "sebastian@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "Waylon Smithers",
     gender: "male",
     title: "Butler",
     email: "waylon@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "Winston 'Jeeves' Smith",
     gender: "male",
     title: "Butler",
     email: "jeeves@botler.com",
     phone: "1235551212",
-  },
-  {
+  }])
+Contact.create!([{
     name: "You There",
     gender: "male",
     title: "Butler",
