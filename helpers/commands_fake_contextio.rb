a505eb12c9abad4f7b8076c7c3fe6e4485f8909b
mpowel <mlp206@gmail.com>
2016-12-11 23:02:20 -0500

    closing script

--- helpers/commands_fake_contextio.rb
+++ helpers/commands_fake_contextio.rb
@@ -14,17 +14,18 @@ module Sinatra
       
       is_admin = is_admin_or_owner client, event
         
-      if event.formatted_text.starts_with? "when"
-         client.chat_postMessage(channel: event.channel, text: "You last contacted Alfred on December 11. He has yet to respond. Would you like to email him now?", as_user: true)
+      if event.formatted_text.starts_with? "when"     #simulates a response to "When was the last time I contacted Alfred Pennyworth"
+         client.chat_postMessage(channel: event.channel, text: "You last contacted Alfred on December 11. He has yet to respond. Type 'email' to email him now or you can ask me another question.", as_user: true)
        return true
 
-      elsif event.formatted_text.starts_with? "how"
-          client.chat_postMessage(channel: event.channel, text: "It's been 1 day since you contacted Jeeves. His average response rate is every 3 days. Type 'set reminder' and I'll remind you to email him in 3 days", as_user: true)
+      elsif event.formatted_text.starts_with? "how"   #simulates a response to "How long has it been since I contacted Jeeves"
+          client.chat_postMessage(channel: event.channel, text: "It's been 1 day since you contacted Jeeves. His average response rate is every 3 days. Type 'set reminder' and I'll remind you to email him in 2 days.", as_user: true)
         return true
         
-      elsif event.formatted_text.starts_with? "set"
-          client.chat_postMessage(channel: event.channel, text: "Very well. I'll remind you to email Jeeves in 3 days", as_user: true)
+      elsif event.formatted_text.starts_with? "set"   #simulates a response to "set reminder"
+          client.chat_postMessage(channel: event.channel, text: "Very well. I'll remind you to email Jeeves in 2 days", as_user: true)
           client.chat_postMessage(channel: event.channel, text: "You mentioned once he can be quite ornary, so be sure to use pleasantries.", as_user: true)
+          client.chat_postMessage(channel: event.channel, text: "Anything else I can do for you today? If not, just say 'bye' and I'll leave you in peace.", as_user: true)
         return true
          
 
