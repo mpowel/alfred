35da617bbfc9ea46dcddac827cb598c1def11ee2
mpowel <mlp206@gmail.com>
2016-12-11 19:46:50 -0500

    syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
+# Contact.create( name: "Alfred Pennyworth", gender: "male", title: "Butler", email: "alfred@botler.com", phone: "1235551212" )
 Contact.create([{
     name: "Alfred Pennyworth",
     gender: "male",
     title: "Butler",
     email: "alfred@botler.com",
-    phone: "1235551212",
+    phone: "1235551212"
     }])
     
     
