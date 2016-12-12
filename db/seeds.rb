fddbe475be6046158fcb1b1f4170eb61d25e3836
mpowel <mlp206@gmail.com>
2016-12-11 19:39:35 -0500

    sytax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,82 +1,82 @@
 Contact.create([{
-    name: "Alfred Pennyworth"
+    name: "Alfred Pennyworth",
     gender: "male",
     title: "Butler",
     email: "alfred@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Butler Livers"
+    name: "Butler Livers",
     gender: "male",
     title: "Butler",
     email: "livers@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Edwin Jarvis"
+    name: "Edwin Jarvis".
     gender: "male",
     title: "Butler",
     email: "jarvis@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Geoffrey Butler"
+    name: "Geoffrey Butler".
     gender: "male",
     title: "Butler",
     email: "geoffrey@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Herbert Arthur Runcible Cadbury"
+    name: "Herbert Arthur Runcible Cadbury",
     gender: "male",
     title: "Butler",
     email: "hcadbury@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Ianto Jones"
+    name: "Ianto Jones",
     gender: "male",
     title: "Butler",
     email: "ianto@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Orto K. Gotchi"
+    name: "Orto K. Gotchi",
     gender: "male",
     title: "Butler",
     email: "orto@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Rosie the Robot Maid"
+    name: "Rosie the Robot Maid",
     gender: "female",
     title: "Maid",
     email: "rosie@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Sebastian"
+    name: "Sebastian",
     gender: "male",
     title: "Butler",
     email: "sebastian@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Waylon Smithers"
+    name: "Waylon Smithers",
     gender: "male",
     title: "Butler",
     email: "waylon@botler.com",
     phone: "1235551212",
   },
   {
-    name: "Winston 'Jeeves' Smith"
+    name: "Winston 'Jeeves' Smith",
     gender: "male",
     title: "Butler",
     email: "jeeves@botler.com",
     phone: "1235551212",
   },
   {
-    name: "You There"
+    name: "You There",
     gender: "male",
     title: "Butler",
     email: "youthere@botler.com",
