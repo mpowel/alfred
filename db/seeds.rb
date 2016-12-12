56ab7f49decec4d370186531b7a045ad6a718562
mpowel <mlp206@gmail.com>
2016-12-11 19:37:52 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,92 @@
-# Contact.create
-# name: "Alfred Pennyworth"
-# name: "Butler Livers"
-# name: "Edwin Jarvis"
-# name: "Geoffrey Butler"
-# name: "Herbert Arthur Runcible Cadbury"
-# name: "Ianto Jones"
-# name: "Orto K. Gotchi"
-# name: "Rosie the Robot Maid"
-# name: "Sebastian Beach"
-# name: "Waylon Smithers"
-# name: "Winston 'Jeeves' Smith"
-# name: "You There"
+Contact.create([{
+    name: "Alfred Pennyworth"
+    gender: "male",
+    title: "Butler",
+    email: "alfred@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Butler Livers"
+    gender: "male",
+    title: "Butler",
+    email: "livers@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Edwin Jarvis"
+    gender: "male",
+    title: "Butler",
+    email: "jarvis@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Geoffrey Butler"
+    gender: "male",
+    title: "Butler",
+    email: "geoffrey@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Herbert Arthur Runcible Cadbury"
+    gender: "male",
+    title: "Butler",
+    email: "hcadbury@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Ianto Jones"
+    gender: "male",
+    title: "Butler",
+    email: "ianto@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Orto K. Gotchi"
+    gender: "male",
+    title: "Butler",
+    email: "orto@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Rosie the Robot Maid"
+    gender: "female",
+    title: "Maid",
+    email: "rosie@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Sebastian"
+    gender: "male",
+    title: "Butler",
+    email: "sebastian@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Waylon Smithers"
+    gender: "male",
+    title: "Butler",
+    email: "waylon@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "Winston 'Jeeves' Smith"
+    gender: "male",
+    title: "Butler",
+    email: "jeeves@botler.com",
+    phone: "1235551212",
+  },
+  {
+    name: "You There"
+    gender: "male",
+    title: "Butler",
+    email: "youthere@botler.com",
+    phone: "1235551212",
+  },
+  }])
+
+
+
+
+
+
+
